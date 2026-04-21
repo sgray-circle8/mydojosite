88e53b2e3ba91d40602bcb6b09a74ec04430f945
sgray <s.gray@circle-eight.com>
2026-04-21 13:03:10 +1200

    Refresh: Add vars for dojo details to SiteConfig

--- app/src/Extensions/SiteConfigExtension.php
+++ app/src/Extensions/SiteConfigExtension.php
@@ -4,15 +4,18 @@ namespace App\Extensions;
 
 use SilverStripe\Core\Extension;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\TextField;
 
 class SiteConfigExtension extends Extension
 {
 
     private static array $db = [
+        'DojoName' => 'Varchar(255)',
+        'DojoAddress' => 'Varchar(255)',
         'ContactEmail' => 'Varchar(255)',
         'FacebookLink' => 'Varchar(255)',
-        'ContactAddress' => 'Varchar(255)',
+        'TrainingSchedule' => 'Text',
     ];
 
     public function updateCMSFields(FieldList $fields): void
@@ -21,6 +24,14 @@ class SiteConfigExtension extends Extension
             'Root.Main',
             [
                 TextField::create(
+                    'DojoName',
+                    'Dojo Name'
+                ),
+                TextField::create(
+                    'DojoAddress',
+                    'Dojo Address'
+                ),
+                TextField::create(
                     'ContactEmail',
                     'Contact Email'
                 ),
@@ -28,9 +39,9 @@ class SiteConfigExtension extends Extension
                     'FacebookLink',
                     'Facebook Link'
                 ),
-                TextField::create(
-                    'ContactAddress',
-                    'Contact Address'
+                TextareaField::create(
+                    'TrainingSchedule',
+                    'Training Schedule'
                 ),
             ],
         );
